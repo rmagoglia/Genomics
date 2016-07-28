03e6bbae3ad564725e674ea1d6762538361cfb54
Rachel Agoglia <rmagoglia@gmail.com>
2016-07-27 23:57:46 -0700

    Debug
    Debugging, added runtime and run statistics.

--- splitSpeciesReads.py
+++ splitSpeciesReads.py
@@ -4,7 +4,7 @@ splitSpeciesReads.py
 Designed to separate reads from the two species in hybrid data.
 Adapted from GetGeneASEbyReads.py by Peter Combs.
 
-Last edit: 07.26.2016
+Last edit: 07.27.2016
 
 """
 
@@ -13,6 +13,7 @@ from pysam import Samfile
 from argparse import ArgumentParser, FileType
 from collections import defaultdict, Counter
 from os import path
+import time
 
 def get_phase(read, snps):
     phase = None
@@ -51,31 +52,49 @@ def get_snps(snpfile):
     return snps
 
 def split_reads(reads, ref_reads, alt_reads):
+    read_results = Counter()
+
     ref_bam = Samfile(ref_reads, 'wb', template=reads)
     alt_bam = Samfile(alt_reads, 'wb', template=reads)
 
     prev_phase = None
     prev_read = None
     prev_qname = None
+    test = 0
 
     for read in reads.fetch(until_eof=True):
+        test += 1
         chrom = read.reference_name
         snps_on_chrom = snp_dict[chrom]
         phase = get_phase(read, snps_on_chrom)
-
         read_qname = read.qname
         if read_qname == prev_qname:
-            if phase == prev_phase:
-                if phase == 1:
+            read_results["tot_read"]+=1
+            phase_set = set([phase, prev_phase])
+            phase_set.discard(None)
+            if len(phase_set) == 1:
+                read_phase = phase_set.pop()
+                if read_phase == 1:
                     ref_bam.write(read)
                     ref_bam.write(prev_read)
-                elif phase == -1:
+                    read_results["ref_read"]+=1
+                elif read_phase == -1:
                     alt_bam.write(read)
                     alt_bam.write(prev_read)
-        
+                    read_results["alt_read"]+=1
+                elif read_phase == 0:
+                    read_results['misphased_read']+=1
+            elif len(phase_set)==0:
+                read_results["no_snps_read"]+=1
+            else:
+                read_results['misphased_read']+=1
+
         prev_read = read
         prev_phase = phase
         prev_qname = read_qname
+
+    return(read_results)
+    
 
 
 def parse_args():
@@ -86,10 +105,27 @@ def parse_args():
     parser.add_argument('alt_reads')
 
     args = parser.parse_args()
-    print(args)
     return args
 
 if __name__ == "__main__":
+    print(time.strftime(("%b %d ") + time.strftime("%I:%M:%S")),"Loading SNPs...")
     args = parse_args()
     snp_dict = get_snps(args.snp_file)
-    split_reads(args.reads, args.ref_reads, args.alt_reads)
+
+    print(time.strftime(("%b %d ") + time.strftime("%I:%M:%S")),"Done with SNPs. Splitting reads...")
+    read_results = split_reads(args.reads, args.ref_reads, args.alt_reads)
+
+    print(time.strftime(("%b %d ") + time.strftime("%I:%M:%S")),"Finished!")
+
+    print()
+    print("RUN STATISTICS:")
+    print("  Total input reads:", str(read_results['tot_read']))
+    print("  Reads with no SNPs:", str(read_results['no_snps_read']), 
+        "(" + "%.2f" % ((read_results['no_snps_read']/float(read_results['tot_read']))*100) + "%)")
+    print("  Reads with misphased SNPs:", str(read_results['misphased_read']), 
+        "(" + "%.2f" % ((read_results['misphased_read']/float(read_results['tot_read']))*100) + "%)")
+    print("  Reference reads:", str(read_results['ref_read']), 
+        "(" + "%.2f" % ((read_results['ref_read']/float(read_results['tot_read']))*100) + "%)")
+    print("  Alternate reads:", str(read_results['alt_read']), 
+        "(" + "%.2f" % ((read_results['alt_read']/float(read_results['tot_read']))*100) + "%)")
+
